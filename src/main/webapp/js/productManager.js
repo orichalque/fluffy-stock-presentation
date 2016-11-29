4121e0d8f7dfb94165e75aba4c6da3c2f146fa98
Thibault BEZIERS-LA-FOSSE <E104607D@S046V7pc11.ensinfo.sciences.univ-nantes.prive>
2016-11-29 10:00:09 +0100

    T - ajout de onSignIn google

--- src/main/webapp/js/productManager.js
+++ src/main/webapp/js/productManager.js
@@ -208,9 +208,19 @@
                 $scope.tomodif="";
             });
         };
+
+
+        $scope.onSignIn=function(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+        };
+        
         /*
          * ==============================================================
-         */
+        */
 
 
 
