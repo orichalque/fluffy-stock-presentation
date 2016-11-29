771416c44dab7e118351729f22e6d116b8fab9e4
Thibault BEZIERS-LA-FOSSE <E104607D@S046V7pc11.ensinfo.sciences.univ-nantes.prive>
2016-11-29 10:04:57 +0100

    T - changement du scope de la fonction js onSignIn

--- src/main/webapp/js/productManager.js
+++ src/main/webapp/js/productManager.js
@@ -209,15 +209,6 @@
             });
         };
 
-
-        $scope.onSignIn=function(googleUser) {
-            var profile = googleUser.getBasicProfile();
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail());
-        };
-        
         /*
          * ==============================================================
         */
@@ -377,3 +368,12 @@
 
 
 })();
+
+
+var onSignIn=function(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+};
