d4ccc551ee9b1f9aea44961ecfe70552ef2072bc
dennisman <dennisman@laposte.net>
2016-12-01 18:00:13 +0100

    D- Documentation added on /doc

--- src/main/java/com/alma/group8/dispatcher/WebConfiguration.java
+++ src/main/java/com/alma/group8/dispatcher/WebConfiguration.java
@@ -26,5 +26,6 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         //Set the welcome page ton index.html
         registry.addViewController("/").setViewName("forward:/index.html");
         registry.addViewController("/api").setViewName("forward:/api.html");
+        registry.addViewController("/doc").setViewName("forward:/doc/overview-summary.html");
     }
 }
